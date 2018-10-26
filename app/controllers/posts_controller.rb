cf9411148e9afec9c920a8454473c059e028d0a5
yushanks <jis_ushan@yahoo.com>
2018-10-26 15:10:38 +0900

    add find posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   def index
-	@posts = Post.all
+	@posts = Post.all.order(created_at: :desc)
   end
 
   def show
-  	@id=params[:id]
+  	@post = Post.find_by(id: params[:id])
 
   end
 end
