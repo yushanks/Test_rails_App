baa16e9b33df3ad3e52965705fde015bd02d440b
yushanks <jis_ushan@yahoo.com>
2018-10-26 15:52:05 +0900

    add create post method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,10 @@ class PostsController < ApplicationController
 
   def new
   end
+
+  def create
+  	@post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/posts/index")
+  end 
 end
