c6b8b5d5216bce90e610675acdd341b9d4e2dd3e
yushanks <jis_ushan@yahoo.com>
2018-10-26 19:00:14 +0900

    user model  migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_26_083002) do
+ActiveRecord::Schema.define(version: 2018_10_26_095704) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
