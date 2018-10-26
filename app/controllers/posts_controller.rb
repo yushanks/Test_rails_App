459fa5e4e0be37861e1640eaa0d7e98dc2bd7c2c
yushanks <jis_ushan@yahoo.com>
2018-10-26 17:00:09 +0900

    post update page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,4 +25,15 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find_by(id: params[:id])
   end
+
+  def update
+  	@post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/edit")
+    end
+  end
 end
