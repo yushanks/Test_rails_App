76cc047939742002d6cc29c1ff06e8fde080723a
yushanks <jis_ushan@yahoo.com>
2018-10-29 12:03:36 +0900

    add new migration file of image to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_29_024059) do
+ActiveRecord::Schema.define(version: 2018_10_29_025846) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_10_29_024059) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
