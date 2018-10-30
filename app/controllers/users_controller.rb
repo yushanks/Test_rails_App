372b11f02f65e04b4591b0e5abfb2f4de4b1187f
yushanks <jis_ushan@yahoo.com>
2018-10-30 18:21:23 +0900

    edit user new to enter password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,16 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+   
   def create
     @user = User.new(
       name: params[:name],
       email: params[:email],
-      image_name: "default_user.jpg"
+      image_name: "default_user.jpg",
+      password: params[:password]
     )
-    if @user.save == true
+    if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
     else
@@ -65,6 +67,12 @@ class UsersController < ApplicationController
       
       render("users/login_form")
     end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get "login" => "users#login_form"
   post "login" => "users#login"
+  post "logout" => "users#logout"
   post "users/:id/update" => "users#update"
   get "users/:id/edit" => "users#edit"
   post "users/create" => "users#create"
