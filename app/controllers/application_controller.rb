d9687a1d45e8008bcf095cf5e680b129bf87a31a
yushanks <jis_ushan@yahoo.com>
2018-10-30 18:33:42 +0900

    forbit login user of logined

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,11 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def forbid_login_user
+    if @current_user
+      flash[:notice] = "すでにログインしています"
+      redirect_to("/posts/index")
+    end
+  end
+
 end
