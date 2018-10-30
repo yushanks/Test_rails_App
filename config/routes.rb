8c4a11fbb4a3b02c72d5e9572175f5b6f3c9731a
yushanks <jis_ushan@yahoo.com>
2018-10-30 17:50:38 +0900

    add login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "login" => "users#login_form"
   post "users/:id/update" => "users#update"
   get "users/:id/edit" => "users#edit"
   post "users/create" => "users#create"
