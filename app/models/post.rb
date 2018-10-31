d46d72e31253586af83f88a56dc63522494959ff
yushanks <jis_ushan@yahoo.com>
2018-10-31 09:31:27 +0900

    show user information

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   validates :content, {presence: true, length: {maximum: 140}}
   validates :user_id, {presence: true}
 　
-
+　#postはユーザーに紐付ける
   def user
     return User.find_by(id: self.user_id)
   end
