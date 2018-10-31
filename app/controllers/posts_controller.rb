0ad848f736b18c4f32e738f0df8fcc6053aefe4e
yushanks <jis_ushan@yahoo.com>
2018-10-31 09:50:48 +0900

    add ensure_correct_user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   
   before_action :authenticate_user
+  before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
   def index
 	@posts = Post.all.order(created_at: :desc)
   end
@@ -45,6 +46,13 @@ class PostsController < ApplicationController
     @post.destroy
     flash[:notice]="投稿を削除しました"
     redirect_to("/posts/index")
-  	
+  end
+
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if @post.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
   end
 end
