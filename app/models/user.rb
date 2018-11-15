5925e274d69c63b590228f27b03b475a7b46e219
yushanks <jis_ushan@yahoo.com>
2018-11-15 17:07:48 +0900

    fixed login method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
   validates :name, {presence: true}
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
-  has_secure_password
   def post
     return Post.where(user_id: self.id)
   end
