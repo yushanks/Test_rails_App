cf91c332123226eeab9617cc7eebd49b7541f38b
yushanks <jis_ushan@yahoo.com>
2018-11-15 17:03:41 +0900

    add password authentication and encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   validates :name, {presence: true}
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
-  validates :password, {presence: true}
-  def posts
+  has_secure_password
+  def post
     return Post.where(user_id: self.id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_31_005700) do
+ActiveRecord::Schema.define(version: 2018_11_15_075755) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2018_10_31_005700) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
